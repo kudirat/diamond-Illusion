4d38208f93326cc3317d39bcc8ef485a0e038491
kudirat <47795311+kudirat@users.noreply.github.com>
2020-10-07 21:46:14 -0500

    Update diamondIllusion.js

--- diamondIllusion.js
+++ diamondIllusion.js
@@ -3,7 +3,8 @@ function setup() {
     var context = canvas.getContext('2d');
     var slider1 = document.getElementById('slider1');
     slider1.value = -25;
-  
+    var posX = -10;
+    var posY = -5;
   
 
     function draw() {
@@ -12,29 +13,36 @@ function setup() {
 	// use the sliders to get the angles
 	var theta1 = slider1.value*0.005*Math.PI;
 	
-	function moveToTx(x,y,Tx)
-	{var res=vec2.create(); vec2.transformMat3(res,[x,y],Tx); context.moveTo(res[0],res[1]);}
-
-	function lineToTx(x,y,Tx)
-	{var res=vec2.create(); vec2.transformMat3(res,[x,y],Tx); context.lineTo(res[0],res[1]);}
-
+    var stack = [ mat3.create() ];
       
-  function drawDiamond(context, x, y, width, height, color,  Tx){
+	 function moveToTx(x,y)  {
+       var res=vec2.create(); 
+       vec2.transformMat3(res,[x,y],stack[0]);
+       context.moveTo(res[0],res[1]);
+     }
+
+	function lineToTx(x,y){
+      var res=vec2.create();
+      vec2.transformMat3(res,[x,y],stack[0]); 
+      context.lineTo(res[0],res[1]);
+    }
+	
+//draws an outline of a diamond
+  function drawDiamond(context, x, y, width, height, color){
    context.save();
    context.beginPath();
-   moveToTx(x, y, Tx);
+   moveToTx(x, y);
                 
-    // top left edge
-   lineToTx(x - width / 2, y + height / 2, Tx);
+    // draws top left edge
+   lineToTx(x - width / 2, y + height / 2);
                 
-  // bottom left edge
-   lineToTx(x, y + height,Tx);
+  // draws bottom left edge
+   lineToTx(x, y + height);
                 
-  // bottom right edge
-   lineToTx(x + width / 2, y + height / 2, Tx);
+  // draws bottom right edge
+   lineToTx(x + width / 2, y + height / 2);
                 
-  // closing the path automatically creates
-  // the top right edge
+  // draws closing the path automatically creates
    context.closePath();
                 
    context.strokeStyle = color;
@@ -43,22 +51,22 @@ function setup() {
   }
       
       
-   function drawDiamondFill(context, x, y, width, height, color,  Tx){
+   function drawDiamondFill(context, x, y, width, height, color){
    context.save();
    context.beginPath();
-   moveToTx(x, y, Tx);
+   moveToTx(x, y);
                 
-    // top left edge
-   lineToTx(x - width / 2, y + height / 2, Tx);
+    // draws top left edge
+   lineToTx(x - width / 2, y + height / 2);
                 
-  // bottom left edge
-   lineToTx(x, y + height,Tx);
+  // draws bottom left edge
+   lineToTx(x, y + height);
                 
-  // bottom right edge
-   lineToTx(x + width / 2, y + height / 2, Tx);
+  // draws bottom right edge
+   lineToTx(x + width / 2, y + height / 2);
                 
-  // closing the path automatically creates
-  // the top right edge
+  
+  // draws the top right edge
    context.closePath();
                 
    context.fillStyle = color;
@@ -66,127 +74,145 @@ function setup() {
    context.restore();
   }
   
-  function drawLine(x, y, height, width, color, Tx){ 
+  function drawLine(x, y, height, width, color){ 
     context.beginPath();
-    moveToTx(10, 30, Tx);
+    moveToTx(10, 30);
     context.rect(x ,y ,height ,width, color);  
     context.fillStyle = color;  
     context.closePath();
     context.fill();
   }   
       
+      function update() {
+        posX += 1;
+        if (posX >= 550) {
+            posX = -85;
+        }
+
+       
+    }
       
+      function updateAgain() {
+        posY -= 1;
+        if (posX >= 550) {
+            posX = -85;
+        }
+    }
  
  var innerDiamondCanvas = mat3.create();
-  mat3.fromTranslation(innerDiamondCanvas,[-10,37]);
+  mat3.fromTranslation(innerDiamondCanvas,[posX,posY]);
 //mat3.rotate(Tgreen_to_blue,Tgreen_to_blue,theta1);
   mat3.scale(innerDiamondCanvas,innerDiamondCanvas, [0.75,0.75]);
-  drawDiamondFill(context, canvas.width * 0.8, 200, 100, 100, "#B537F2", innerDiamondCanvas);
-    
-  //draw middle diamond     
-  var blueToInnerDiamond = mat3.create();
- mat3.fromTranslation(blueToInnerDiamond,[-320,-250]);
+  mat3.multiply(stack[0],stack[0],innerDiamondCanvas);
+  drawDiamondFill(context, canvas.width * 0.8, 200, 100, 100, "#B537F2");
+   
+   //draw middle diamond      
+ stack.unshift(mat3.clone(stack[0]));        
   var bluDiamondCanvas = mat3.create();
-  mat3.scale(blueToInnerDiamond,blueToInnerDiamond, [2,2]);
-  mat3.multiply(bluDiamondCanvas, innerDiamondCanvas, blueToInnerDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#3CB9FC", bluDiamondCanvas);
- 
-    //draw first diamond on right  
-  var purpToBlueDiamond = mat3.create();
-  mat3.fromTranslation(purpToBlueDiamond,[20,0]);
-	//mat3.rotate(Tgreen_to_blue,Tgreen_to_blue,theta1);
+  mat3.fromTranslation(bluDiamondCanvas,[-320,-250]);
+  mat3.scale(bluDiamondCanvas,bluDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0], stack[0], bluDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#3CB9FC");
+  stack.shift();
+      
+  //draw first diamond on right  
+  stack.unshift(mat3.clone(stack[0]));     
   var purpDiamondCanvas = mat3.create();
-  mat3.multiply(purpDiamondCanvas, bluDiamondCanvas, purpToBlueDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#B537F2", purpDiamondCanvas);
-    
+  mat3.fromTranslation(purpDiamondCanvas,[-285,-250]);
+  mat3.scale(purpDiamondCanvas,purpDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0], stack[0], purpDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#B537F2");
+  stack.shift();  
+      
   //draw first diamond on left
-  var drkpurpToBlueDiamond = mat3.create();
-  mat3.fromTranslation(drkpurpToBlueDiamond,[-20,0]);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
+  stack.unshift(mat3.clone(stack[0]));   
   var drkpurpDiamondCanvas = mat3.create();
-  //mat3.scale(drkpurpToBlueDiamond,drkpurpToBlueDiamond, [1.5,1.5]);
-  mat3.multiply(drkpurpDiamondCanvas,bluDiamondCanvas,drkpurpToBlueDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#120052", drkpurpDiamondCanvas);
-   
+  mat3.fromTranslation(drkpurpDiamondCanvas,[-355,-250]);
+  mat3.scale(drkpurpDiamondCanvas,drkpurpDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0],stack[0],drkpurpDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#120052");
+  stack.shift();  
+      
  //draw second diamond on left
-  var lilacTodrkpurpDiamond = mat3.create();
-  mat3.fromTranslation(lilacTodrkpurpDiamond,[-20,0]);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
+ stack.unshift(mat3.clone(stack[0]));   
   var lilacDiamondCanvas = mat3.create();
-  //mat3.scale(drkpurpToBlueDiamond,drkpurpToBlueDiamond, [1.5,1.5]);
-  mat3.multiply(lilacDiamondCanvas,drkpurpDiamondCanvas,lilacTodrkpurpDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#8A2BE2", lilacDiamondCanvas);
-   
+  mat3.fromTranslation(lilacDiamondCanvas,[-395,-250]);
+  mat3.scale(lilacDiamondCanvas,lilacDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0],stack[0],lilacDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#8A2BE2");
+  stack.shift();
+      
   //draw second diamond on Right
-  var gldTopurpDiamond = mat3.create();
-  mat3.fromTranslation(gldTopurpDiamond,[20,0]);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
+  stack.unshift(mat3.clone(stack[0]));   
   var gldDiamondCanvas = mat3.create();
-  //mat3.scale(drkpurpToBlueDiamond,drkpurpToBlueDiamond, [1.5,1.5]);
-  mat3.multiply(gldDiamondCanvas,purpDiamondCanvas,gldTopurpDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#FFD300", gldDiamondCanvas);
-   
+  mat3.fromTranslation(gldDiamondCanvas,[-250,-250]);
+  mat3.scale(gldDiamondCanvas,gldDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0],stack[0],gldDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#FFD300");
+  stack.shift();
+      
   //draw second diamond on left
-  var greenTolilacDiamond = mat3.create();
-  mat3.fromTranslation(greenTolilacDiamond,[-20,0]);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
+  stack.unshift(mat3.clone(stack[0]));   
   var greenDiamondCanvas = mat3.create();
-  //mat3.scale(drkpurpToBlueDiamond,drkpurpToBlueDiamond, [1.5,1.5]);
-  mat3.multiply(greenDiamondCanvas,lilacDiamondCanvas,greenTolilacDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#00FECA", greenDiamondCanvas);
-       
+  mat3.fromTranslation(greenDiamondCanvas,[-430,-250]);
+  mat3.scale(greenDiamondCanvas,greenDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0],stack[0],greenDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#00FECA");
+  stack.shift();  
       
  //draw second diamond on right
-  var pinkToGldDiamond = mat3.create();
-  mat3.fromTranslation(pinkToGldDiamond,[20,0]);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
+  stack.unshift(mat3.clone(stack[0]));   
   var pinkDiamondCanvas = mat3.create();
-  //mat3.scale(drkpurpToBlueDiamond,drkpurpToBlueDiamond, [1.5,1.5]);
-  mat3.multiply(pinkDiamondCanvas,gldDiamondCanvas,pinkToGldDiamond);
-  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#FF2281", pinkDiamondCanvas);
-       
- var topLineToPink = mat3.create();
-  mat3.fromTranslation(topLineToPink,[20,0]);
-  //doesn't work :(
-  mat3.rotate(topLineToPink, topLineToPink, theta1);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
-  var topLineToCanvas = mat3.create();
-   mat3.multiply(topLineToCanvas,pinkDiamondCanvas,topLineToPink);
-  drawLine(330, 80, 100, 4, "black", topLineToCanvas);
- 
- var btmLineToPink = mat3.create();
-  mat3.fromTranslation(btmLineToPink,[20,0]);
-  //doesn't work :(
-  mat3.rotate(btmLineToPink, btmLineToPink, theta1);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
-  var btmLineToCanvas = mat3.create();
-   mat3.multiply(btmLineToCanvas,pinkDiamondCanvas,btmLineToPink);
-  drawLine(330, 300, 100, 4, "black", topLineToCanvas);   
+  mat3.fromTranslation(pinkDiamondCanvas,[-215,-250]);
+  mat3.scale(pinkDiamondCanvas,pinkDiamondCanvas, [2,2]);
+  mat3.multiply(stack[0],stack[0],pinkDiamondCanvas);
+  drawDiamond(context, canvas.width * 0.8, 200, 100, 100, "#FF2281");
+   stack.shift();
       
-  var topLineToGreen = mat3.create();
-  mat3.fromTranslation(topLineToGreen,[-20,0]);
+ stack.unshift(mat3.clone(stack[0]));       
+ var topLineToCanvas = mat3.create();
+  mat3.fromTranslation(topLineToCanvas,[20,0]);
   //doesn't work :(
-  mat3.rotate(topLineToGreen, topLineToGreen, theta1);
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
+  mat3.rotate(topLineToCanvas, topLineToCanvas, theta1);
+   mat3.multiply(stack[0],stack[0],topLineToCanvas);
+  drawLine(330, 80, 100, 4, "black");
+ stack.shift();
+      
+  stack.unshift(mat3.clone(stack[0]));   
+ var btmLineToCanvas = mat3.create();
+  mat3.fromTranslation(btmLineToCanvas,[20,0]);
+  //doesn't work :(
+  mat3.rotate(btmLineToCanvas, btmLineToCanvas, theta1);
+   mat3.multiply(stack[0],stack[0],btmLineToCanvas);
+  drawLine(330, 300, 100, 4, "black");   
+     stack.shift();
+      
+  stack.unshift(mat3.clone(stack[0]));   
   var topLineToCanvasLft = mat3.create();
-   mat3.multiply(topLineToCanvasLft,greenDiamondCanvas,topLineToGreen);
-  drawLine(20, 80, 70, 4, "black", topLineToCanvasLft);   
-      
-      
-  var btmLineToGreen = mat3.create();
-  mat3.fromTranslation(btmLineToGreen,[-20,0]);
+  mat3.fromTranslation(topLineToCanvasLft,[-20,0]);
   //doesn't work :(
-  
-  //mat3.rotate(Tred_to_green,Tred_to_green,phi1);
-  var btmLineToCanvasLft = mat3.create();
-   mat3.multiply(btmLineToCanvasLft,greenDiamondCanvas,btmLineToGreen);
-  drawLine(20, 300, 70, 4, "black", btmLineToCanvasLft);   
- btmLineToCanvasLft.rotate(btmLineToCanvasLft, btmLineToCanvasLft, theta1);
-  
+  mat3.rotate(topLineToCanvasLft, topLineToCanvasLft, theta1);
+   mat3.multiply(stack[0],stack[0],topLineToCanvasLft);
+  drawLine(20, 80, 70, 4, "black");   
+     stack.shift(); 
       
+      
+   stack.unshift(mat3.clone(stack[0]));   
+  var btmLineToCanvasLft = mat3.create();
+  mat3.fromTranslation(btmLineToCanvasLft,[posX,posY]);
+  //doesn't work :(
+ //btmLineToCanvasLft.rotate(btmLineToCanvasLft, btmLineToCanvasLft, theta1);
+   mat3.multiply(stack[0],stack[0],btmLineToCanvasLft);
+  drawLine(20, 300, 70, 4, "black");   
+  stack.shift();
+      
+      update();
+      //updateAgain();
+    window.requestAnimationFrame(draw);
    
     }
-    slider1.addEventListener("input",draw);
+  window.requestAnimationFrame(draw);  
+  slider1.addEventListener("input",draw);
    
     draw();
 }
