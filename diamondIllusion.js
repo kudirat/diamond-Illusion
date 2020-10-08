bcb095a1d124f8dff927883593e709a33114405e
kudirat <47795311+kudirat@users.noreply.github.com>
2020-10-07 21:46:56 -0500

    Update diamondIllusion.js

--- diamondIllusion.js
+++ diamondIllusion.js
@@ -1,8 +1,8 @@
 function setup() {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    var slider1 = document.getElementById('slider1');
-    slider1.value = -25;
+   // var slider1 = document.getElementById('slider1');
+    //slider1.value = -25;
     var posX = -10;
     var posY = -5;
   
@@ -10,8 +10,8 @@ function setup() {
     function draw() {
  
 	canvas.width = canvas.width;
-	// use the sliders to get the angles
-	var theta1 = slider1.value*0.005*Math.PI;
+	
+	//var theta1 = slider1.value*0.005*Math.PI;
 	
     var stack = [ mat3.create() ];
       
@@ -212,7 +212,7 @@ function setup() {
    
     }
   window.requestAnimationFrame(draw);  
-  slider1.addEventListener("input",draw);
+ // slider1.addEventListener("input",draw);
    
     draw();
 }
